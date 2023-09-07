7bc15d42bd4fa993e22ac077eb37b167da5b4f68
Heaven <94477820+h1vn@users.noreply.github.com>
2023-09-07 14:00:46 +0200

    Update ft_is_negative.c

--- C00/ex04 -  ft_is_negative/ft_is_negative.c
+++ C00/ex04 -  ft_is_negative/ft_is_negative.c
@@ -9,8 +9,8 @@ void ft_is_negative(int n) {
 }
 
 int main() {
-    ft_is_negative(5);
+    ft_is_negative(1);
     ft_is_negative(0);
-    ft_is_negative(-5);
+    ft_is_negative(-1);
     return 0;
 }
