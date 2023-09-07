86ea1ec520a7a2c6d2ede1b2a0de2c7191287e36
Heaven <94477820+h1vn@users.noreply.github.com>
2023-09-07 12:46:03 +0200

    Update ft_print_alphabet.c

--- C00/ex01 - ft_print_alphabet.c/ft_print_alphabet.c
+++ C00/ex01 - ft_print_alphabet.c/ft_print_alphabet.c
@@ -1,15 +1,12 @@
 #include <unistd.h>
 
 void ft_print_alphabet() {
-  char lettreA = 'a';
-
-  while (lettreA <= 'z') {
-    putchar(lettreA);
-    lettreA++;
-  }
+    for (char lettreA = 'a'; lettreA <= 'z'; lettreA++) {
+        write(1, &lettreA, 1);
+    }
 }
 
 int main() {
-  ft_print_alphabet();
-  return 0;
+    ft_print_alphabet();
+    return 0;
 }
