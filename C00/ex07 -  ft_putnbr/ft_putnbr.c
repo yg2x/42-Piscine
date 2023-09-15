2527aa3e935bde5d8afcedd11f3983779567c634
93cz <94477820+93cz@users.noreply.github.com>
2023-09-15 11:02:42 +0200

    Update ft_putnbr.c

--- C00/ex07 -  ft_putnbr/ft_putnbr.c
+++ C00/ex07 -  ft_putnbr/ft_putnbr.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruda-sil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 10:31:23 by ruda-sil          #+#    #+#             */
+/*   Updated: 2023/09/15 10:34:38 by ruda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(char c) {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
 void ft_putnbr(int nb) {
-    if (nb < 0) {
-        ft_putchar('-');
-        nb = -nb;
-    }
+	if (nb < 0) {
+		ft_putchar('-');
+		nb = -nb;
+	}
 
-    if (nb >= 10) {
-        ft_putnbr(nb / 10);
-    }
+	if (nb >= 10) {
+		ft_putchar(nb / 10);
+	}
 
-    ft_putchar('0' + (nb % 10));
+	ft_putchar('0' + (nb % 10));
 }
 
 int main() {
-    ft_putnbr(42);
-    return 0;
+	ft_putchar(42);
+	return 0
 }
